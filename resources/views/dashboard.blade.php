4e5d56fac274e6d328ca4caf0362f51a6250b5c1
sloc-dev <daniele.montesi@gmail.com>
2018-06-14 17:45:42 +0200

    .

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,40 +7,28 @@
 	<script type="text/javascript" src="{{ asset('js/controllers/clienti.js') }}"></script>
 @endsection
 @section('body_content')
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-		<a class="navbar-brand js-scroll-trigger" href="#page-top">
-			<span class="d-block d-lg-none">SlocFit</span>
-			<span class="d-none d-lg-block">
-				<img class="img-fluid img-profile rounded-circle mx-auto mb-2" ng-src="@{{ gravatar_url }}" alt="Gravatar Image">
-			</span>
-		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav">				
-				<li class="nav-item pt-1 pb-1" ng-class="{ 'active': activeMenu == 'home' }">
-					<a class="nav-link js-scroll-trigger" href="#/home">
-						<i class="fas fa-home pr-2"></i> Home
-					</a>
-				</li>
-				<li class="nav-item pt-1 pb-1" ng-class="{ 'active': activeMenu == 'clienti' }">
-					<a class="nav-link js-scroll-trigger" href="#/clienti">
-						<i class="fas fa-users pr-2"></i> Clienti
-					</a>
-				</li>
-				<li class="nav-item pt-1 pb-1" ng-class="{ 'active': activeMenu == 'palestra' }">
-					<a class="nav-link js-scroll-trigger" href="#/palestra">
-						<i class="fas fa-building pr-2"></i> Palestra
-					</a>
-				</li>
-				<li class="nav-item pt-1 pb-1" ng-class="{ 'active': activeMenu == 'impostazioni' }">
-					<a class="nav-link js-scroll-trigger" href="#/impostazioni">
-						<i class="fas fa-cog pr-2"></i> Impostazioni
-					</a>
-				</li>
-				<!--
-					<ul class="navbar-nav ml-auto">
+	<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item" ng-class="{ 'active': activeMenu == 'home' }">
+						<a class="nav-link" href="#/home">Home</a>
+					</li>
+					<li class="nav-item" ng-class="{ 'active': activeMenu == 'clienti' }">
+						<a class="nav-link" href="#/clienti">Clienti</a>
+					</li>
+					<li class="nav-item" ng-class="{ 'active': activeMenu == 'palestra' }">
+						<a class="nav-link" href="#/palestra">Palestra</a>
+					</li>
+					<li class="nav-item" ng-class="{ 'active': activeMenu == 'impostazioni' }">
+						<a class="nav-link" href="#/impostazioni">Impostazioni</a>
+					</li>				
+				</ul>
+				<ul class="navbar-nav ml-auto">
 					<li class="nav-item dropdown">
 						<a class="nav-link" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							<img class="user-avatar rounded-circle" ng-src="@{{ gravatar_url }}" alt="Gravatar Image">
@@ -51,14 +39,8 @@
 						</div>
 					</li>
 				</ul>
-				-->
-			</ul>
+			</div>
 		</div>
 	</nav>
-	<div class="container-fluid p-0">
-		<section class="p-3 p-lg-4 d-flex flex-column">
-			<h2 ng-bind="title"></h2>
-			<div ng-view></div>
-		</section>
-	</div>
+	<div class="container mt-5 pt-4" ng-view></div>
 @endsection
