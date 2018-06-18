94c3046ed46ed20b50ef9a02e156461adac71d30
sloc-dev <40240103+sloc-dev@users.noreply.github.com>
2018-06-18 09:05:24 +0200

    Update home.js

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -1,5 +1,17 @@
 app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
 	
 	$scope.tab = 'statistiche';
+  	$scope.count = 0;
+  
+  	$scope.initCount = function(){
+		$http.get('/api/clienti/count')
+		.success(function (data, status, headers, config) {
+			$scope.count = data.data.count;
+		}).error(function (data, status, headers, config) {
+			UtilsSvc.notifyError(data.response);
+		});
+	};
+  
+  	$scope.initCount();
 	
 }]);
