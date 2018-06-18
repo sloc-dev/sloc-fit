1890c305b4180265f94dad608b04ae92784aaed6
sloc-dev <40240103+sloc-dev@users.noreply.github.com>
2018-06-18 11:08:17 +0200

    Update clienti.js

--- public/js/controllers/clienti.js
+++ public/js/controllers/clienti.js
@@ -35,6 +35,7 @@ app.controller('ClientiCtrl', ['$scope', '$http', 'UtilsSvc', function ($scope, 
           	var index = $scope.clienti.indexOf($scope.cliente);
 			$scope.clienti[index].id = data.data.id;
 			$scope.cliente = null;
+          	$scope.page = 'list';
 			UtilsSvc.notifySuccess(data.response);
 		}).error(function (data, status, headers, config) {
 			UtilsSvc.notifyError(data.response);
